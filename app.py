b5e19ac2542e84fa8cd6773d12338f0f1d6ab25d
Adey-Bee <mradeybee@gmail.com>
2015-07-16 08:10:30 +0100

    Edit and Delete done, some finishing works underway

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #this is used to import clases from flask 	`
 from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from flask.ext.sqlalchemy import SQLAlchemy
-from functools  import wraps
-from models import *
+from functools import wraps
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+
+# from forms import SearchForm
+# from config import MAX_SEARCH_RESULTS
 # import sqlite3
 #this is the application object
 app = Flask(__name__)
@@ -12,12 +17,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 	
 
 db = SQLAlchemy(app)
-
+from models import *
+migrate = Migrate(app, db)
+ 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 # from models import *
 # these are the routes, they specify the path to the urls
-
+def login_required(f):
+	@wraps(f)
+	def wrap(*args,**kwargs):
+		if 'logged_in' in session:
+			return f(*args,**kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('admin'))
+	return wrap		
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
 	users = db.session.query(User).order_by(User.name)
 	return render_template("dashboard.html", users=users)#this is a tempalte page
@@ -40,7 +58,6 @@ def home():
 		db.session.commit()
 		flash('Registration Successfull.')
 	return render_template('home.html',error = error)
-	return render_template("home.html")#this is a tempalte page
 
 
 
@@ -56,11 +73,8 @@ def admin():
 			return redirect(url_for('dashboard'))
 	return render_template('admin.html', error=error)#this is a tempalte page
 
-
-
-
-
 @app.route('/logout')
+@login_required
 def logout():
 	session.pop('logged_in', None)
 	flash('You have been logged out')
@@ -79,23 +93,51 @@ def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
 
+@app.route('/edit.<id>', methods=['POST', 'GET'])
+@login_required
+def edit (id):
+    #Getting user by primary key:
+    users = User.query.get(id)
+    if request.method == 'POST':		
+		name = request.form['name']
+		phone_number = request.form['phone_number']
+		email = request.form['email']
+		address = request.form['address']
+		table = request.form['table']
+		user_reg = User(name,phone_number,email,address,table)
+		db.session.update(user_reg)
+		db.session.commit()
+		flash ('Edited')
+		return  redirect(url_for('dashboard'))
+    return render_template('edit.html', users=users)
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
-    form = EditForm(g.user.nickname)
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit'))
-    elif request.method != "POST":
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html', form=form)
+@app.route('/delete/<id>' , methods=['POST', 'GET'])
+@login_required
+def delete(id):
+	user = User.query.get(id)
+	db.session.delete(user)
+	db.session.commit()
+	flash ('Deleted')
+	return redirect(url_for('dashboard'))
+
+
+# @app.route('/edit', methods=['GET', 'POST'])
+# def edit():
+#     form = EditForm(g.user.nickname)
+#     if form.validate_on_submit():
+#         g.user.nickname = form.nickname.data
+#         g.user.about_me = form.about_me.data
+#         db.session.add(g.user)
+#         db.session.commit()
+#         flash('Your changes have been saved.')
+#         return redirect(url_for('edit'))
+#     elif request.method != "POST":
+#         form.nickname.data = g.user.nickname
+#         form.about_me.data = g.user.about_me
+#     return render_template('edit.html', form=form)
 
 @app.route('/admin_insert', methods=['GET', 'POST'])
+@login_required
 def insert():
 	error = None
 	if request.method == 'POST':
@@ -113,7 +155,31 @@ def insert():
 		db.session.commit()
 		flash('Registration Successfull.')
 	return render_template('admin_insert.html',error = error)
-	return render_template("admin_insert.html")#this is a tempalte page
+	
+
+# @app.before_request
+# def before_request():
+#     g.user = current_user
+#     if g.user.is_authenticated():
+#         g.user.last_seen = datetime.utcnow()
+#         db.session.add(g.user)
+#         db.session.commit()
+#         g.search_form = SearchForm()
+
+# @app.route('/search', methods=['POST'])
+
+# def search():
+#     if not g.search_form.validate_on_submit():
+#         return redirect(url_for('home'))
+#     return redirect(url_for('search_results', query=g.search_form.search.data))
+
+# @app.route('/search_results/<query>')
+
+# def search_results(query):
+#     results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
+#     return render_template('search_results.html',
+#                            query=query,
+#                            results=results)
 
 if __name__ == '__main__':
 	app.run(debug = True) 
--- models.py
+++ models.py
@@ -1,4 +1,8 @@
 from app import db
+
+
+# import sys
+
 class User(db.Model):
 	__tablename__ = "users"
 	id = db.Column(db.Integer, primary_key=True)
@@ -16,5 +20,24 @@ class User(db.Model):
 		self.table = table
 
 
-	def __repr__(self):
-		return '<name{}'.format(self.name)
+
+# if sys.version_info >= (3, 0):
+#     enable_search = False
+# else:
+#     enable_search = True
+#     import flask.ext.whooshalchemy as whooshalchemy
+
+# class Post(db.Model):
+#     __searchable__ = ['name', 'email', 'phone_number']
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), primary_key=False)
+#     phone_number = db.Column(db.String(100), primary_key=False)
+#     email = db.Column(db.String(100), primary_key=False)
+#     address = db.Column(db.String(100), primary_key=False)
+#     table = db.Column(db.String(100), primary_key=False)
+def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+# if enable_search:
+#     whooshalchemy.whoosh_index(app, Post)
